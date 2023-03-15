84edf5cf3348a30340ff13db5f0e2111fea31131
shadowasphodel2919 <t2905j@gmail.com>
2023-03-15 11:16:38 +0530

    Added

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials} from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3600',
+    baseUrl: 'https://techdome.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
