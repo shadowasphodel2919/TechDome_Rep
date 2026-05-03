a89c3736df372446403f30c06002e8750e79f403
shadowasphodel2919 <t2905j@gmail.com>
2026-05-04 01:55:40 +0530

    major overhaul

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://techdome.vercel.app',
+    process.env.FRONTEND_URL || 'https://techdome.vercel.app',
     'https://techdom.onrender.com'
 ]
 
