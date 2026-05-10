286d0e15c6a6439070bdac0cadb32a950bb493a8
shadowasphodel2919 <t2905j@gmail.com>
2026-05-10 23:35:50 +0530

    cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://techdome.vercel.app',
-    'https://techdome.onrender.com'
+    'https://techdome.onrender.com',
+    'https://skillorbit.tahajamal.dev'
 ]
 
 module.exports = allowedOrigins
